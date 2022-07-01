db878d0f4e79e444818362f5be0b2afd140d2798
wrm244 <wrm244@outlook.com>
2022-07-02 00:53:53 +0800

    按钮点击事件

--- Code04/app/src/main/java/com/example/code04/MainActivity.java
+++ Code04/app/src/main/java/com/example/code04/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.code04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final EditText etMessage = findViewById(R.id.message);
+        Button btSent = findViewById(R.id.send_message);
+        btSent.setOnClickListener(view -> {
+            String message = etMessage.getText().toString().trim();
+            Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+        });
     }
 }
