75b0aa1f7ad23242ae39de00614958bfc55b3430
wrm244 <wrm244@outlook.com>
2022-07-02 09:22:55 +0800

    code4完成

--- Code04/app/src/main/java/com/example/code04/MainActivity.java
+++ Code04/app/src/main/java/com/example/code04/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.code04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String MESSAGE_STRING=
+            "com.example.code4.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,9 @@ public class MainActivity extends AppCompatActivity {
         Button btSent = findViewById(R.id.send_message);
         btSent.setOnClickListener(view -> {
             String message = etMessage.getText().toString().trim();
-            Toast.makeText(MainActivity.this,message,
-                    Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this,MessageActivity.class);
+            intent.putExtra(MESSAGE_STRING,message);
+            startActivity(intent);
         });
     }
 }
--- Code04/app/src/main/java/com/example/code04/MessageActivity.java
+++ Code04/app/src/main/java/com/example/code04/MessageActivity.java
@@ -2,7 +2,9 @@ package com.example.code04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        TextView tvMessage = findViewById(R.id.message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MESSAGE_STRING);
+        if(message!=null){
+            if(tvMessage!=null){
+                tvMessage.setText(message);
+            }
+        }
     }
 }
