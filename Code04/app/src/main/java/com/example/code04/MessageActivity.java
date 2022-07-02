7aaec1a20db08523cb505260391a4d6572b908e9
wrm244 <wrm244@outlook.com>
2022-07-02 09:36:16 +0800

    code4完成

--- Code04/app/src/main/java/com/example/code04/MessageActivity.java
+++ Code04/app/src/main/java/com/example/code04/MessageActivity.java
@@ -13,7 +13,7 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
         TextView tvMessage = findViewById(R.id.message);
-        Intent intent = new Intent();
+        Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.MESSAGE_STRING);
         //获取键值对为“MainActivity.MESSAGE_STRING”的value
         if(message!=null){
