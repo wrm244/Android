0b978bced548b152b0784ed49436fb8ef0b8591f
wrm244 <wrm244@outlook.com>
2022-07-02 16:52:22 +0800

    计数器2 commit_first

--- Code02/app/src/main/java/com/example/code02/MainActivity.java
+++ Code02/app/src/main/java/com/example/code02/MainActivity.java
@@ -5,10 +5,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private int count = 0;
+    TextView tvCount;
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count=savedInstanceState.getInt(COUNT_VALUE);
+        if(tvCount!=null){
+            tvCount.setText(Integer.toString(count));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +28,15 @@ public class MainActivity extends AppCompatActivity {
         Button btnShowToast = findViewById(R.id.btnShowToast);
         btnShowToast.setOnClickListener(view ->
                 Toast.makeText(MainActivity.this,"Hello Word",Toast.LENGTH_SHORT).show());
-
-
-        final TextView tvCount = findViewById(R.id.tvCount);
+        //final TextView tvCount = findViewById(R.id.tvCount);
         Button btnCount = findViewById(R.id.btnCount);
         btnCount.setOnClickListener(view ->
                 tvCount.setText(Integer.toString(++count)));
-
+    }
+    private static final String COUNT_VALUE = "count_value";
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(COUNT_VALUE,count);
+        super.onSaveInstanceState(outState);
     }
 }
