f178ad86b46e1360ba58b48754ba52a2401606cd
wrm244 <wrm244@outlook.com>
2022-07-02 17:23:41 +0800

    实验2.2 生命周期

--- Code02/app/src/main/java/com/example/code02/MainActivity.java
+++ Code02/app/src/main/java/com/example/code02/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.code02;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,16 +12,15 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private int count = 0;
     TextView tvCount;
-
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d("onRestoreInstanceState","onRestoreInstanceStateStart");
         super.onRestoreInstanceState(savedInstanceState);
         count=savedInstanceState.getInt(COUNT_VALUE);
         if(tvCount!=null){
             tvCount.setText(Integer.toString(count));
         }
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +28,18 @@ public class MainActivity extends AppCompatActivity {
         Button btnShowToast = findViewById(R.id.btnShowToast);
         btnShowToast.setOnClickListener(view ->
                 Toast.makeText(MainActivity.this,"Hello Word",Toast.LENGTH_SHORT).show());
-        //final TextView tvCount = findViewById(R.id.tvCount);
+        tvCount = findViewById(R.id.tvCount);
         Button btnCount = findViewById(R.id.btnCount);
         btnCount.setOnClickListener(view ->
                 tvCount.setText(Integer.toString(++count)));
+
     }
     private static final String COUNT_VALUE = "count_value";
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d("onRestoreInstanceState","onSaveInstanceStateStart");
         outState.putInt(COUNT_VALUE,count);
         super.onSaveInstanceState(outState);
     }
+
 }
