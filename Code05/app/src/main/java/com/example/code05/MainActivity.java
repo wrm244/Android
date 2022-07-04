40fecaa2dbf791d2bfeec090260ff4ff3b1b74e7
wrm244 <wrm244@outlook.com>
2022-07-04 15:16:36 +0800

    密码明文切换

--- Code05/app/src/main/java/com/example/code05/MainActivity.java
+++ Code05/app/src/main/java/com/example/code05/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.code05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private Boolean bPwdSwitch = false;
+    private EditText etPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView ivPwdSwitch = findViewById(R.id.iv_pwd_switch);
+        etPwd = findViewById(R.id.et_pwd);
+
+        ivPwdSwitch.setOnClickListener(new View.OnClickListener() {
+
+            //修改密码的显示与隐藏
+            @Override
+            public void onClick(View view) {
+                bPwdSwitch = !bPwdSwitch;
+                if (bPwdSwitch) {
+                    ivPwdSwitch.setImageResource(
+                            R.drawable.ic_baseline_visibility_off_24);
+                    etPwd.setInputType(
+                            InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                } else {
+                    ivPwdSwitch.setImageResource(
+                            R.drawable.ic_baseline_visibility_off_24);
+                    etPwd.setInputType(
+                            InputType.TYPE_TEXT_VARIATION_PASSWORD |
+                                    InputType.TYPE_CLASS_TEXT);
+                    etPwd.setTypeface(Typeface.DEFAULT);
+                }
+            }
+        });
+
     }
 }
